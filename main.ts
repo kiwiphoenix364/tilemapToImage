9d21da9399489fa94aad5186cdb0b63d4799b309
kiwiphoenix <kiwiphoenix364@gmail.com>
2024-08-28 18:03:06 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,26 @@
 namespace tilemapToImg {
     //% block
     //% block="generate image from  tilemap $inputTilemap to image $outputImage"
-    export function generateImage(inputTilemap: any, outputImage: any) {
-        for (let x = 0; x < inputTilemap.width; x++) {
-            for (let y = 0; y < inputTilemap.height; y++) {
-                helpers.imageBlit(outputImage, x * 16, x * 16, x * 16 + 15, x * 16 + 15, inputTilemap.tileImageAtLocation(inputTilemap.getTileLocation(x, y)), 0, 0, 16, 16, true, false)
+    export function generateImage(inputTilemap: any) {
+        let outputImage = image.create(inputTilemap.width, inputTilemap.height)
+        for (let x = 0; x < inputTilemap.width / 16; x++) {
+            for (let y = 0; y < inputTilemap.height / 16; y++) {
+                helpers.imageBlit(
+                outputImage, 
+                x * 16, 
+                x * 16, 
+                x * 16 + 15, 
+                x * 16 + 15, 
+                inputTilemap.tileImageAtLocation(inputTilemap.getTileLocation(x, y)), 
+                0, 
+                0, 
+                16, 
+                16, 
+                true, 
+                false
+                )
             }
         }
+        return outputImage
     }
 }
