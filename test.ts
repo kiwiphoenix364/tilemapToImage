33e6fb6060828e527a74c2c876d36f6b504ca379
kiwiphoenix <kiwiphoenix364@gmail.com>
2024-09-23 23:28:25 -0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,1 @@
 // tests go here; this will not be compiled when this package is used as an extension.
-
-let image1 = tilemapToImg.generateImage(tilemap`level1`)
-scene.setBackgroundImage(image1)
-scene.setBackgroundColor(1)
