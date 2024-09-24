d410c0f6d16b87ed9fbbc7a59e55b41fbf21fed5
kiwiphoenix <kiwiphoenix364@gmail.com>
2024-09-23 23:28:02 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,10 @@
 namespace tilemapToImg {
-    //% block
-    //% block="generate image from  tilemap $inputTilemap to image $outputImage"
-    export function generateImage(inputTilemap: any) {
-        let outputImage = image.create(inputTilemap.width, inputTilemap.height)
-        for (let x = 0; x < inputTilemap.width / 16; x++) {
-            for (let y = 0; y < inputTilemap.height / 16; y++) {
-                helpers.imageBlit(
-                outputImage, 
-                x * 16, 
-                x * 16, 
-                x * 16 + 15, 
-                x * 16 + 15, 
-                inputTilemap.tileImageAtLocation(inputTilemap.getTileLocation(x, y)), 
-                0, 
-                0, 
-                16, 
-                16, 
-                true, 
-                false
-                )
+    //% block="generate image from  tilemap $inputTilemap"
+    export function generateImage(inputTilemap: tiles.TileMapData) {
+        let outputImage = image.create(inputTilemap.width * 16, inputTilemap.height * 16)
+        for (let x = 0; x < inputTilemap.width; x++) {
+            for (let y = 0; y < inputTilemap.height; y++) {
+                helpers.imageBlit(outputImage, x * 16, y * 16, 16, 16, inputTilemap.getTileImage(inputTilemap.getTile(x, y)), 0, 0, 16, 16, true, false)
             }
         }
         return outputImage
--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+
+let image1 = tilemapToImg.generateImage(tilemap`level1`)
+scene.setBackgroundImage(image1)
+scene.setBackgroundColor(1)
